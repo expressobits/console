a14a2122c1f61890694d2a9b07ad4addf7573ad1
Rafael Correa <scriptsengineer@gmail.com>
2023-05-28 12:02:21 -0300

    Move to module

---
+++ console/plugin.gd
@@ -0,0 +1,13 @@
+@tool
+extends EditorPlugin
+
+const DefaultActions = preload("./DefaultActions.gd")
+
+
+const PLUGIN_NAME = 'Console'
+const PLUGIN_PATH = 'res://addons/quentincaffeino/console/src/Console.tscn'
+
+
+func _enter_tree():
+#	self.add_autoload_singleton(PLUGIN_NAME, PLUGIN_PATH)
+	DefaultActions.register()
---
+++ console/src/Type/IntType.gd
@@ -0,0 +1,13 @@
+
+extends 'res://addons/quentincaffeino/console/src/Type/BaseRegexCheckedType.gd'
+
+
+func _init():
+	super('Int', '^[+-]?\\d+$')
+
+
+# @param    Variant  value
+# @returns  int
+func normalize(value):
+	return value
+#	return int(self._reextract(value))
